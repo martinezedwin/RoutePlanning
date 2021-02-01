6ae302335408b5e286598801a937c3b618d43acd
Edwin Martinez <edwin.martinez@plus.ai>
2021-02-01 12:00:04 -0800

    TO DO 1 DONE

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
         return std::nullopt;
     
     auto size = is.tellg();
-    std::vector<std::byte> contents(size);    
+    std::vector<std::byte> contents(size);
     
     is.seekg(0);
     is.read((char*)contents.data(), size);
@@ -52,15 +52,33 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
+    // [DONE] TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+
+    float start_x, start_y, end_x, end_y;
+
+    std::cout << "Input start x" << std::endl;
+    std::cin >> start_x;
+
+    std::cout << "Input start y" << std::endl;
+    std::cin >> start_y;
+
+    std::cout << "Input end x" << std::endl;
+    std::cin >> end_x;
+
+    std::cout << "Input end y" << std::endl;
+    std::cin >> end_y;
+
+    std::cout << "Start Position: " << "(" << start_x <<"," << start_y <<")" << "\n";
+    std::cout << "End Position: " << "(" << end_x <<"," << end_y <<")" << "\n";
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    // RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y , end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
