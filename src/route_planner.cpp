0887dd52143598d284187a605f39728e73eab823
Edwin Martinez <edwin.martinez@plus.ai>
2021-02-01 14:38:04 -0800

    attempt at TODO 2

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,6 +10,9 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
+    RoutePlanner::start_noede = &my_Model.FindClosestNode(start_x, start_y);
+    RoutePlanner::end_node = &my_Model.FindClosestNode(end_x, end_y);
+
 
 }
 
